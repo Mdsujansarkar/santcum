68227b43214a1141cd01aa392cafe542ad94b100
Mdsujansarkar <sujansarkar786@gmail.com>
2021-05-29 12:26:57 +0600

    controller move

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/products', function () {
-    return "Products";
-});
+Route::get('/products', [ProductController::class,'index']);
+
+Route::post('/products', [ProductController::class,'store']);
 
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
