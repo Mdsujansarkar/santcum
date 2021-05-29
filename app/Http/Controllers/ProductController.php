2c7f1406622fe877c3ef16d385b55a04cc973ded
Mdsujansarkar <sujansarkar786@gmail.com>
2021-05-29 18:34:19 +0600

    complite

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -35,6 +35,12 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
+        $request->validate([
+            'name' =>'required',
+            'slug' =>'required',
+            'description' =>'required',
+            'price' =>'required',
+        ]);
         return Product::create($request->all());
     }
 
@@ -46,7 +52,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        return Product::find($id);
     }
 
     /**
@@ -69,7 +75,10 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+        $product->update($request->all());
+
+        return $product;
     }
 
     /**
@@ -80,6 +89,17 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+       return $product = Product::destroy($id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($name)
+    {
+       return Product::where('name', 'like', '%' .$name. '%')->get();
     }
 }
